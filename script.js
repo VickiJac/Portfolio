c4bf8c7be166f6e22ed3e4d30a450afef4f53f86
DeathDefying <vickijacobson@hotmail.com>
2023-06-28 19:17:48 +0200

    Add Skills Section

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let aboutMe = document.getElementById('about-me');
 let careerEducation = document.getElementById('career-education');
+let portfolio = document.getElementById('portfolio');
+let skills = document.getElementById('skills');
 
 aboutMe.hidden = true;
 careerEducation.hidden = true;
+portfolio.hidden = true;
+skills.hidden = true;
+
